c9fc3ed92efe63624fe7497b5a73c44fe3b4a4ee
Paul Anderson <paul@Pauls-MacBook-Pro.local>
2020-07-10 14:16:41 -0700

    initial version

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pablo0615/myMath
+
+go 1.12
---
+++ myMath.go
@@ -0,0 +1,10 @@
+// myMath.go - myMath module
+package myMath
+
+func Add(a, b int) int { return a + b }
+
+func Sub(a, b int) int { return a - b }
+
+func Mul(a, b int) int { return a * b }
+
+func Div(a, b int) int { return a / b }
