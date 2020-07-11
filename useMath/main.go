d267a23544e06738cf70a9918431d84e8050660e
pablo0615 <53460760+pablo0615@users.noreply.github.com>
2020-07-11 09:28:05 -0700

    Update main.go

--- useMath/main.go
+++ useMath/main.go
@@ -2,19 +2,19 @@
 package main
 import (
     "fmt"
-    gm "github.com/pablo0615/myMath"
-    gmv2 "github.com/pablo0615/myMath/v2"
+    v1 "github.com/pablo0615/myMath"
+    v2 "github.com/pablo0615/myMath/v2"
     )
 
 func main() {
-    add := gmv2.Add(1, 2, 3, 4, 5, 6)
+    add := v2.Add(1, 2, 3, 4, 5, 6)
     fmt.Println(add)
-    if div, err := gm.Div(40, 5); err != nil {
+    if div, err := v1.Div(40, 5); err != nil {
         fmt.Println(err)
     } else {
         fmt.Println(div)
     }
-    pow := gm.Pow(3, 4)
+    pow := v1.Pow(3, 4)
     fmt.Println(pow)
 }
 
